a32652068ca8769f751312e6939faeb3d4ff038b
Gihara Wicktramatunge <giharanw@gmail.com>
2022-01-24 23:24:04 +0530

    first commit

---
+++ src/controller/ManageCustomerFormController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class ManageCustomerFormController {
+
+    public Button btnNewCustomer;
+    public TextField txtId;
+    public TextField txtFirstName;
+    public TextField txtLastName;
+    public DatePicker txtDob;
+    public TextField txtPicture;
+    public Button btnBrowse;
+    public TextField txtTelephone;
+    public Button btnAdd;
+    public ListView<?> lstTelephone;
+    public Button btnRemove;
+    public Button btnSaveCustomer;
+    public TableView<?> tblCustomers;
+
+    public void initialize(){
+
+    }
+
+    @FXML
+    void btnAdd_OnAction(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void btnBrowse_OnAction(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void btnNewCustomer_OnAction(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void btnRemove_OnAction(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void btnSaveCustomer_OnAction(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void txtTelephone_OnAction(ActionEvent event) {
+
+    }
+
+}
